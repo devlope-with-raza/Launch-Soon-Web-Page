594f5d259b45c28c0908cde6f3750835ec97ab14
devlope-with-raza <razakazi011@gmail.com>
2024-12-29 20:01:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+var CountDownDate = new Date("oct 16, 2027 00:00:00").getTime();
+var x = setInterval(function(){
+    var now = new Date().getTime();
+    var distance = CountDownDate - now;
+    
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+var hours = Math.floor((distance % (1000 * 60 * 60 * 24 )) / (1000 * 60 * 60));
+var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+document.getElementById("days").innerHTML = days;
+document.getElementById("hours").innerHTML = hours;
+document.getElementById("minutes").innerHTML = minutes;
+document.getElementById("seconds").innerHTML = seconds;
+
+
+
+
+},1000);
